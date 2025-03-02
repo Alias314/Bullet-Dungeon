7ae96316493dd27fb3e3829fc98e1fef3ec39975
Alias314 <83777973+Alias314@users.noreply.github.com>
2025-03-02 14:08:50 +0800

    Adjusted level layout for now

--- src/Environment/LevelLayout.jsx
+++ src/Environment/LevelLayout.jsx
@@ -8,8 +8,13 @@ export default function LevelLayout({ layout, playerRef }) {
       {layout.map((row, i) =>
         row.map((room, j) =>
           room === 0 ? (
-            <StartingRoom
+            // <StartingRoom
+            //   position={[(j - 3) * cellSize, 0, (i - 3) * cellSize]}
+            //   playerRef={playerRef}
+            // />
+            <EmptyRoom
               position={[(j - 3) * cellSize, 0, (i - 3) * cellSize]}
+              amountEnemy={0}
               playerRef={playerRef}
             />
           ) : room === 1 ? (
