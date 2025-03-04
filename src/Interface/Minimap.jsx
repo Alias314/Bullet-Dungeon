8eabbda8fdc10fc43e0d75cfa53c2229c0c8e7c9
Shawn Mayol <mayolshawn@gmail.com>
2025-03-04 22:27:47 +0800

    Revert "style: format code with Prettier"
    This reverts commit f8c875f193d2188b53f48efb44cee24ebda13d67.

--- src/Interface/Minimap.jsx
+++ src/Interface/Minimap.jsx
@@ -1,22 +1,22 @@
 export default function Minimap({ layout }) {
-    return (
-        <div className="absolute top-2 right-2 border-2 border-white">
-            {layout.map((row, rowIndex) => (
-                <div key={rowIndex} className="flex">
-                    {row.map((room, colIndex) => (
-                        <div
-                            key={colIndex}
-                            className={`w-8 h-8 flex items-center justify-center transparent opacity-80 ${
-                                room === 0
-                                    ? "bg-amber-500"
-                                    : room === 1
-                                    ? "bg-red-500"
-                                    : "bg-gray-900"
-                            }`}
-                        ></div>
-                    ))}
-                </div>
-            ))}
+  return (
+    <div className="absolute top-2 right-2 border-2 border-white">
+      {layout.map((row, rowIndex) => (
+        <div key={rowIndex} className="flex">
+          {row.map((room, colIndex) => (
+            <div
+              key={colIndex}
+              className={`w-8 h-8 flex items-center justify-center transparent opacity-80 ${
+                room === 0
+                  ? "bg-amber-500"
+                  : room === 1
+                  ? "bg-red-500"
+                  : "bg-gray-900"
+              }`}
+            ></div>
+          ))}
         </div>
-    );
+      ))}
+    </div>
+  );
 }
