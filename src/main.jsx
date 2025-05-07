fe5f44855c71646f77b5aa6895c21193dc5bd665
Alias314 <83777973+Alias314@users.noreply.github.com>
2025-05-07 16:43:53 +0800

    fixed import problem in main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { preloadModels } from "./Interface/Logic/PreloadModels.jsx";
-
-preloadModels();
+import Prel from "./Interface/Logic/PreloadModels.jsx";
+import "./Interface/Logic/PreloadModels.jsx"
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
